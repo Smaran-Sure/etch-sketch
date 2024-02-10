b006ce687129ea1a598d5e4101d345dc22ff8e7f
Smaran Sure <smaran.sure@gmail.com>
2024-02-09 22:15:08 -0800

    Format sections, remove ugly

--- javascript.js
+++ javascript.js
@@ -15,9 +15,19 @@ function createBoard() {
     }
 }
 createBoard();
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 function pixelDraw() {
     let pixelSize = (640/val) + "px";
     let pixels = document.querySelectorAll(".pixel");
+    let rgb = document.querySelector("#rgb");
+    rgb.addEventListener("click",() => {
+        pixels.forEach((pixel) => pixel.addEventListener("mouseover",() => {
+            pixel.style.backgroundColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`;
+        }))
+    })
+        
     pixels.forEach((pixel) => {
         pixel.style.width = pixelSize;
         pixel.style.height = pixelSize;
@@ -29,7 +39,7 @@ function pixelDraw() {
 pixelDraw();
 reset.addEventListener("click", () => {
     val = parseInt(prompt("Enter an amount of rows/columns", "16"));
-    while (val < 0 || val > 100 || (typeof val != "number")) {
+    while (val <= 0 || val > 100 || (typeof val != "number")) {
         val = parseInt(prompt("Invalid input. Rows & Columns > 0 & < 100 & has to be a number", "16"));
     }
     grid.parentNode.removeChild(grid);
@@ -38,4 +48,3 @@ reset.addEventListener("click", () => {
 })
 
 
-console.log(typeof val)
